ec615da0400ff627882fe3e7d43b4954be778f05
ashleshsortee <sorteeashlesh@gmail.com>
2024-09-01 17:29:36 +0530

    feat: add transfer functionality

--- src/pages/transfer.tsx
+++ src/pages/transfer.tsx
@@ -6,17 +6,17 @@ import { parseEther } from "viem";
 import { JsonRpcSigner } from "ethers";
 import axios from "axios";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 const Transfer = () => {
   const [amount, setAmount] = useState("");
   const [recipient, setRecipient] = useState("");
   const { data: hash, writeContract: approveContract } = useWriteContract();
   const { data: tx, writeContract: deposit } = useWriteContract();
+  const { data: tx1, writeContract: transfer } = useWriteContract();
   // const { data: signer } = useEthersSigner();
-
-  const erc20Address = "0x4a669e1267f6da8b51e21bc3c402a8614ae7cd1a";
-  const zetoTokenAddress = "0x26366dd4C51b84490e01233A491Af142a6Ab96Ba";
+  // const erc20Address = "0x4a669e1267f6da8b51e21bc3c402a8614ae7cd1a";
+  // const zetoTokenAddress = "0x26366dd4C51b84490e01233A491Af142a6Ab96Ba";
+  const erc20Address = "0xBD1dF8b2eeB22ae2f6cB039a1191878eE06583C5";
+  const zetoTokenAddress = "0x2CE73d5A63c948CeFdf2E9ad3b414471c35c905C";
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -47,14 +47,43 @@ const Transfer = () => {
         address: zetoTokenAddress,
         abi: zetoTokenAbi,
         functionName: "deposit",
+        // args: [Number(amountInWei), outputCommitments[0], encodedProof],
         args: [amount, outputCommitments[0], encodedProof],
       });
 
       // Prepare transfer proofs
-      // const transferRes = await axios.post(
-      //   "http://localhost:3001/api/transfer-proof",
-      //   { recipient, amount }
+      const transferRes = await axios.get(
+        "http://localhost:3001/api/transfer-proof"
+      );
+      const transferProof = await transferRes.data;
+
+      const inputCommitments = transferProof.inputCommitments;
+      const outputCommitmentsOfTransfer = transferProof.outputCommitments;
+      const encodedProofOfTransfer = transferProof.encodedProof;
+
+      // const outputOwnerAddresses = transferProof.owners.map(
+      //   (owner: any) => owner.ethAddress
       // );
+
+      console.log(
+        "transfer Proof",
+        inputCommitments,
+        outputCommitmentsOfTransfer,
+        JSON.stringify(encodedProofOfTransfer)
+      );
+
+      const txn = await transfer({
+        address: zetoTokenAddress,
+        abi: zetoTokenAbi,
+        functionName: "transfer",
+        args: [
+          inputCommitments,
+          outputCommitmentsOfTransfer,
+          encodedProofOfTransfer,
+        ],
+      });
+
+      console.log("console tx1", txn);
     } catch (err) {
       console.log("errrr", err);
     }
